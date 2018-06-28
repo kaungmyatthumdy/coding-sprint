97a1f9ffe4a2c85f3040dad8fa9d01cf78b8206f
kaungmyatthumdy <khathtan3131997@gmail.com>
2018-06-28 09:25:13 +0630

    Accessing Elements of Lists

---
+++ ex34.py
@@ -0,0 +1,6 @@
+animals = ['bear', 'tiger','penguin','zebra']
+bear = animals[0]
+print(bear)
+
+animals = ['bear','sharp','peacock','kangaroo','whale','platypus']
+print(animals[0],animals[2],animals[4])
