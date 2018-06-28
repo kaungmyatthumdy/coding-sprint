8ade157a5feb4332874c234488f846466b4f9ea4
kaungmyatthumdy <khathtan3131997@gmail.com>
2018-06-28 09:01:04 +0630

    More Practice

--- ex24.py
+++ ex24.py
@@ -26,17 +26,17 @@ def secret_formula(started):
     return jelly_beans, jars, crates
 
 
-    start_point = 10000
-    beans, jars, crates = secret_formula(start_point)
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
 
-    # remember that this is another way to format a string
-    print("With a starting point of: {}".format(start_point))
-    # it's just like with an f"" string
-    print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
+# remember that this is another way to format a string
+print("With a starting point of: {}".format(start_point))
+# it's just like with an f"" string
+print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 
-    start_point = start_point / 10
+start_point = start_point / 10
 
-    print("We can also do that this way:")
-    formula = secret_formula(start_point)
-    # this is an easy way to apply a list to a format string
-    print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
+print("We can also do that this way:")
+formula = secret_formula(start_point)
+# this is an easy way to apply a list to a format string
+print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
